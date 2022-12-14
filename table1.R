70ab06607935227e847ec1972b38e521a9085541
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2022-12-14 15:47:47 +0100

    add energy and fiber intake to table1

--- table1.R
+++ table1.R
@@ -11,6 +11,7 @@ df <- readRDS("data/demographics_BEAM.RDS")
 officebp <- readRDS("data/officebp_summary.RDS")
 officebp_v2 <- officebp %>% filter(visit == "V2") %>% select(-visit)
 bia <- readRDS("data/bia_data.RDS")
+diet <- readRDS("data/diet_summary.RDS")
 bmi <- bia %>% filter(visit == "V2") %>% select(ID, BMI)
 lab <- readRDS("data/lab_results.RDS")
 lab_v2 <- lab %>% filter(visit == "V2") %>% select(ID, GFR, TC, HDL, LDL, TG)
@@ -20,6 +21,7 @@ df_total <- right_join(df, officebp_v2, by = "ID")
 df_total <- right_join(df_total, bmi, by = "ID")
 df_total <- right_join(df_total, lab_v2, by = "ID")
 df_total <- right_join(df_total, nexfin_v2, by = "ID")
+df_total <- right_join(df_total, diet, by = "ID")
 head(df_total)
 names(df_total)
 df_total <- df_total %>% filter(! ID %in% c("BEAM_664", "BEAM_713", "BEAM_299"))
@@ -27,13 +29,15 @@ df_total <- df_total %>% filter(! ID %in% c("BEAM_664", "BEAM_713", "BEAM_299"))
 # Table 1
 table1 <- df_total %>% 
     select(Age, Sex, BMI, Smoking, 
-           # VG? DM, CVD?
+           # VG? CVD?
            BPlowMed, 
            # BPlowMed specs
            Systolic, Diastolic, Pulse, # this is baseline office BP
            # V1_Systolic, V1_Diastolic, V1_Pulse, # this is screening BP
            # Nexfin
-           GFR, TC, HDL, LDL, TG, Treatment_group) %>% 
+           GFR, TC, HDL, LDL, TG, 
+           Energy, Fibers,
+           Treatment_group) %>% 
     CreateTableOne(data=., strata = 'Treatment_group', test = TRUE, addOverall = TRUE) %>% 
     print()
 write.csv2(table1, "results/table1.csv")
